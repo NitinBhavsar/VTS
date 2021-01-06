0c1ae09fb9e0d4cfafefb3d940e29dd537d5c42f
Nitin Bhavsar <42445419+NitinBhavsar@users.noreply.github.com>
2021-01-06 14:42:40 +0530

    Updated User Controller
    Updated User Controller,Repository.

---
+++ UserController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VTSAPI.Models;
+using VTSAPI.Repository;
+using System.Web;
+using System.Web.Http;
+
+namespace VTSAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _userRepository.GetUsers();
+            return new OkObjectResult(users);
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var user = _userRepository.GetUserByID(id);
+            return new OkObjectResult(user);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _userRepository.InsertUser(user);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = user.UserID }, user);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] User user)
+        {
+            if (user != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _userRepository.UpdateUser(user);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepository.DeleteUser(id);
+            return new OkResult();
+        }
+
+        //[Route("user/PostUserImage")]
+        //[AllowAnonymous]
+        //public async Task<HttpResponseMessage> PostUserImage()
+        //{
+        //    Dictionary<string, object> dict = new Dictionary<string, object>();
+        //    try
+        //    {
+
+        //        var httpRequest = HttpContext.Current.Request;
+
+        //        foreach (string file in httpRequest.Files)
+        //        {
+        //            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+        //            var postedFile = httpRequest.Files[file];
+        //            if (postedFile != null && postedFile.ContentLength > 0)
+        //            {
+
+        //                int MaxContentLength = 1024 * 1024 * 1; //Size = 1 MB  
+
+        //                IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".gif", ".png" };
+        //                var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+        //                var extension = ext.ToLower();
+        //                if (!AllowedFileExtensions.Contains(extension))
+        //                {
+
+        //                    var message = string.Format("Please Upload image of type .jpg,.gif,.png.");
+
+        //                    dict.Add("error", message);
+        //                    return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+        //                }
+        //                else if (postedFile.ContentLength > MaxContentLength)
+        //                {
+
+        //                    var message = string.Format("Please Upload a file upto 1 mb.");
+
+        //                    dict.Add("error", message);
+        //                    return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+        //                }
+        //                else
+        //                {
+
+
+
+        //                    var filePath = HttpContext.Current.Server.MapPath("~/Userimage/" + postedFile.FileName + extension);
+
+        //                    postedFile.SaveAs(filePath);
+
+        //                }
+        //            }
+
+        //            var message1 = string.Format("Image Updated Successfully.");
+        //            return Request.CreateErrorResponse(HttpStatusCode.Created, message1); ;
+        //        }
+        //        var res = string.Format("Please Upload a image.");
+        //        dict.Add("error", res);
+        //        return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var res = string.Format("some Message");
+        //        dict.Add("error", res);
+        //        return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+        //    }
+        //}
+
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VTSAPI.DBContexts;
+using VTSAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VTSAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly VITSContext _dbContext;
+
+        public UserRepository(VITSContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void DeleteUser(int vehicleId)
+        {
+            var vehicle = _dbContext.Users.Find(vehicleId);
+            _dbContext.Users.Remove(vehicle);
+            Save();
+        }
+
+        public User GetUserByID(int vehicleId)
+        {
+            return _dbContext.Users.Find(vehicleId);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _dbContext.Users.ToList();
+        }
+
+        public void InsertUser(User vehicle)
+        {
+            _dbContext.Add(vehicle);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateUser(User vehicle)
+        {
+            _dbContext.Entry(vehicle).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
